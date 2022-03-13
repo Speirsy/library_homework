9743bbc0e5f0eb49a25a07ae5c8d1b9319076c9a
Speirsy <jpspeirs@icloud.com>
2022-03-13 20:27:35 +0000

    getting there...

--- library.py
+++ library.py
@@ -42,9 +42,9 @@ while option != "q":
 
     if option == "1":
         print("Listing all books...")
-        # Done - List all books
-        # print(library["books"])
-        # not very pretty. 
+               
+                # print(library["books"])
+                # not very pretty. 
         
     
         for book in library["books"]:
@@ -55,19 +55,15 @@ while option != "q":
         # TODO - Search for a book by title
 
         book_ask = input( "What is the title of the book you wish to search?")
-        title_scan = library["books"] #how to access "titles"????? is it all in the Brackets?
-        for title in title_scan:
-            if title == book_ask:
-                print("yes")
+        books = library["books"]
+        for book in books:
+            if book["title"] == book_ask:
+                print("Yes we have the book")
             else: 
-                print("no")
+                print("Not in library")
 
 
-        # print(title_scan)
-        # if book_title == library["books"][0]:
-        #     print("Book found")
-        # else:
-        #     print("Book not found")
+    
 # You definitely need a loop, you should check each book! 
 # The rest of the logic (input/if statement) is perfect, 
 # but the if/else should be done in a for loop :slightly_smiling_face:
@@ -76,13 +72,38 @@ while option != "q":
     if option == "3":
         print("Adding a book...")
         # TODO - Add a book
+        author_in = input( "Who is the author of the book you wish to add?")
+        title_in = input( "What is the title of the book you wish to add?")
+        library["books"].append({"author":author_in, "title":title_in})
+        #It works!!! How / Where is the added data being stored?
+
 
     if option == "4":
-        print("Removing a book...")
+        print("Removing a book...") 
         # TODO - Remove a book
+        # print books list adding index number. Can't figure this out. 
+        # idea being to ask user for a number to select the item (book) to be removed.
+        for book in library["books"]:
+         print(f"{book['title']} by {book['author']}")
+        
+        # borrow loop from option 2 and 
+        book_ask = input( "What is the title of the book you wish to remove?")
+        books = library["books"] 
+        for book in books:
+            if book["title"] == book_ask:
+                books.remove(book)
+                print(book["title"]) 
+                print("by")
+                print(book["author"])
+                print("is removed")
+                
+        
+            # print(f"{book['title']} by {book['author']} is removed")        
+                #not ideal removing book by it's title but I'll take it!
 
     if option == "5":
         print("Updating a book...")
         # TODO - Update a book
+        book_ask = input( "What is the title of the book you wish to update?")
 
 
